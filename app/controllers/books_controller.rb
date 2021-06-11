6840dd5c8a4e3f67f399de724695c6f61ce2870e
yui-0326 <maeda.yuiha.infratop@gmail.com>
2021-06-11 12:35:58 +0000

    create view file

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,17 @@
+class BooksController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_111939) do
+ActiveRecord::Schema.define(version: 2021_06_11_114646) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
